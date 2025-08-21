95975cf77ac8330f93784bb9b724484917bdc576
John Sims <johnsims.dev@gmail.com>
2025-08-21 10:07:46 -0400

    finish review and nullish coalescing lesson

---
+++ ExecuteProgram/Everyday_TypeScript/2_level/nullishCoalescing.ts
@@ -0,0 +1,5 @@
+function nameOrDefault(name: string | undefined): string {
+    return name ?? 'Wilford';
+}
+
+// return the first value that isn't null or undefined
---
+++ ExecuteProgram/Everyday_TypeScript/2_level/review/01_review.ts
@@ -0,0 +1,18 @@
+
+// type HasEmail = {
+//     email: string
+// };
+// type CanBeAdmin = {
+//     admin: boolean
+// };
+// type User = HasEmail & CanBeAdmin;
+
+// let amir: User = {
+//     email: 'amir@example.com',
+//     admin: true,
+// };
+
+// function extractAdmin(hasEmail: HasEmail): boolean {
+//     return hasEmail.admin;
+// }
+// extractAdmin(amir);
